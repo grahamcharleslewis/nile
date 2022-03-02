e0af6ad827da05ea3da541eb46959f6e9eb6d7cc
Graham Lewis <grahamcharleslewis@gmail.com>
2022-03-02 12:06:06 +0000

    Stage 2 : Initial commit

--- app/controllers/sales_controller.rb
+++ app/controllers/sales_controller.rb
@@ -25,7 +25,7 @@ class SalesController < ApplicationController
 
     respond_to do |format|
       if @sale.save
-        format.html { redirect_to products_path, notice: "Sale was successfully created." }
+        format.html { render @sale.product }
         format.json { render :show, status: :created, location: @sale }
       else
         format.html { render :new, status: :unprocessable_entity }
